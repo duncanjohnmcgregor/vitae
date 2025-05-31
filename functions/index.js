efe0864c1a4b3546d56a748315a169700627f1b3
Duncan <duncanjohnmcgregor@gmail.com>
2025-05-31 12:12:58 +0200

    fixed linting error

--- functions/index.js
+++ functions/index.js
@@ -350,7 +350,7 @@ exports.setAdminClaim = onRequest((req, res) => {
     
     // Get admin secret from environment config
     const functions = require("firebase-functions");
-    const ADMIN_SECRET = functions.config().admin?.secret || process.env.ADMIN_SECRET || "vitae-admin-secret-2024";
+    const ADMIN_SECRET = functions.config().admin?secret || process.env.ADMIN_SECRET || "vitae-admin-secret-2024";
     
     if (secretKey !== ADMIN_SECRET) {
       res.status(401).json({error: "Invalid secret key"});
